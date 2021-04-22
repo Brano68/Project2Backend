4f30f2c552a29ffecb010fa63557b0ae1c63aae1
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-04-22 15:29:19 +0200

    test GET request

---
+++ CreditCard/CreditCard/APIRequests/Controllers/CardController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRequests.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CardController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Payment made successfully"};
+        }
+    }
+}
