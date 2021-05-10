6a926c15d558259e5a63a17614d0779d4531a0e2
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-05-10 12:40:58 +0200

    add post request

--- CreditCard/CreditCard/APIRequests/Controllers/CardController.cs
+++ CreditCard/CreditCard/APIRequests/Controllers/CardController.cs
@@ -1,21 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using APIRequests.DTO;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Newtonsoft.Json.Linq;
+using System.Security.Claims;
+
 
 namespace APIRequests.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("card")]
     public class CardController : ControllerBase
     {
-
-        [HttpGet]
-        public IEnumerable<string> Get()
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("pay")]
+        public async Task<ActionResult<CardDTO>> PostCard([FromBody] CardDTO cardDTO)
         {
-            return new string[] { "Payment made successfully"};
+            int cardNumber = cardDTO.CardNumber;
+            int expirationDate = cardDTO.ExpirationDate;
+            int cvc = cardDTO.Cvc;
+            int sum = cardDTO.Sum;
+
+            return null;
+
         }
+
     }
 }
---
+++ CreditCard/CreditCard/APIRequests/DTO/CardDTO.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRequests.DTO
+{
+    public class CardDTO
+    {
+        [JsonRequired]
+        public int CardNumber { get; set; }
+
+        [JsonRequired]
+        public int ExpirationDate { get; set; }
+
+        [JsonRequired]
+        public int Cvc { get; set; }
+
+        [JsonRequired]
+        public int Sum { get; set; }
+
+    }
+}
