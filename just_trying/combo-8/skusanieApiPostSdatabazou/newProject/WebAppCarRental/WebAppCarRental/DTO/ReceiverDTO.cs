111615399d7b6b27ea53ee3b1b4aae885aece495
Branislav Nebus <branislav.nebus@kosickaakademia.sk>
2021-06-04 19:37:31 +0200

    added a new migration Path for car and global password into the file

---
+++ just_trying/combo-8/skusanieApiPostSdatabazou/newProject/WebAppCarRental/WebAppCarRental/DTO/ReceiverDTO.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCarRental.DTO
+{
+    public class ReceiverDTO
+    {
+        [JsonRequired]
+        public string Login { get; set; }
+        [JsonRequired]
+        public int CarID { get; set; }
+
+        [JsonRequired]
+        public int UserID { get; set; }
+
+        [JsonRequired]
+        public string From { get; set; }
+
+        [JsonRequired]
+        public string To { get; set; }
+
+        [JsonRequired]
+        public double PricePerDays { get; set; }
+
+    }
+}
---
+++ just_trying/combo-8/skusanieApiPostSdatabazou/newProject/WebAppCarRental/WebAppCarRental/Migrations/ContosoCarReservation/20210530095754_updateOfCar.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppCarRental.Migrations.ContosoCarReservation
+{
+    public partial class updateOfCar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AirConditioner",
+                table: "Cars",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Consumption",
+                table: "Cars",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Fuel",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gear",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfPassenger",
+                table: "Cars",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AirConditioner",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "Consumption",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "Fuel",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "Gear",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "NumberOfPassenger",
+                table: "Cars");
+        }
+    }
+}
